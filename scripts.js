58840194014c0786dd08f17c331f8e2749db6bb1
Brévan <81383790+brevan29@users.noreply.github.com>
2024-07-12 15:14:53 +0200

    Stickysation de la navbar

--- scripts.js
+++ scripts.js
@@ -7,7 +7,28 @@ function changervisibilite(elm){
         Actif=AChanger;
 }
 
-if (window.innerWidth<=796){
+function fixNav(){
+    if(window.scrollY > nav.offsetHeight + 25){
+        nav.classList.add('active')
+        titre.style.width='136px'
+        mto.style.width=(nav.offsetWidth-136).toString()+'px'
+        // matin.style.width=((nav.offsetWidth-136)/3).toString()+'px'
+        // apm.style.width=((nav.offsetWidth-136)/3).toString()+'px'
+        // soir.style.width=(mto.offsetWidth-matin.offsetWidth-apm.offsetWidth).toString()+'px'
+    }
+    else{
+        nav.classList.remove('active')
+        titre.style.width='188px'
+        mto.style.width=(nav.offsetWidth-190).toString()+'px'
+        // matin.style.width=((nav.offsetWidth-136)/3).toString()+'px'
+        // apm.style.width=((nav.offsetWidth-136)/3).toString()+'px'
+        // soir.style.width=(mto.offsetWidth-matin.offsetWidth-apm.offsetWidth).toString()+'px'
+    }
+}
+
+
+
+if (window.innerWidth<=500){
     var image = (window.innerWidth-30)/3;
     document.getElementById('Quimper').style.height=(image*3+63).toString()+'px';
     document.getElementById('BZH').style.height=(image*3+63).toString()+'px';
@@ -16,3 +37,8 @@ if (window.innerWidth<=796){
     document.getElementById('BZH').style.overflow="hidden";
     document.getElementById('France').style.overflow="hidden";
 }
+
+const mto = document.querySelector('.meteo_du_jour')
+const titre = document.querySelector('#titre')
+const nav = document.querySelector('#entete')
+window.addEventListener('scroll', fixNav);
