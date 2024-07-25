fc7f34d9bb9086a4b6c5f25703a4980670449e9b
Brévan Hamel <brevanhamel29150@gmail.com>
2024-07-26 00:16:21 +0200

    Trop de changement dont intégration d'un fichier json

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
-var Actif=document.getElementById('Quimper')
+let Actif=document.getElementById('Quimper')
 function changervisibilite(elm){
     const AChanger=document.getElementById(elm)
-    if (AChanger.style.visibility=='hidden'){
+    if (AChanger.style.visibility==='hidden'){
         Actif.style.visibility='hidden';
         AChanger.style.visibility='visible';}
         Actif=AChanger;
 }
-var Actif2=document.getElementById('Horoscope')
+let Actif2=document.getElementById('Horoscope')
 function changervisibiliteTenplus(elm){
     const AChanger=document.getElementById(elm)
-    if (AChanger.style.visibility=='hidden'){
+    if (AChanger.style.visibility==='hidden'){
         Actif2.style.visibility='hidden';
-        AChanger.style.visibility='visible';}
+        AChanger.style.visibility='visible';
+        // Ajustement de la taille de toutes les pages en + pour rendre plus jouli
+        AChanger.style.height = 'fit-content';
+        const heiyt = AChanger.offsetHeight;
+        let divs = document.getElementsByClassName("en-plus")
+        for (i=0; i<divs.length; i++){
+            divs[i].style.height = heiyt.toString()+'px'
+        }}
         Actif2=AChanger;
 }
 function fixNav(){
@@ -26,14 +33,16 @@ function fixNav(){
         mto.style.width=(nav.offsetWidth-192).toString()+'px'
     }
 }
-function creerSoleil(){
-let ech_soleil = document.getElementsByClassName("Soleil")
-for (i=0; i < ech_soleil.length; i++){
-    ech_soleil[i].src="Images/Soleil.svg"
-}}
+function creerTemps(quand, temps){
+    console.log(quand)
+let échéance = getElementsById(quand).getElementsByTagName('img')
+let extension = ".png"
+if (temps === "Soleil"){extension = '.svg'}
+échéance.src="Images/"+temps+extension
+}
 
 if (window.innerWidth<=500){
-    var image = (window.innerWidth-30)/3;
+    let image = (window.innerWidth-30)/3;
     document.getElementById('Quimper').style.height=(image*3+63).toString()+'px';
     document.getElementById('BZH').style.height=(image*3+63).toString()+'px';
     document.getElementById('France').style.height=(image*3+63).toString()+'px';
@@ -41,8 +50,84 @@ if (window.innerWidth<=500){
     document.getElementById('BZH').style.overflow="hidden";
     document.getElementById('France').style.overflow="hidden";
 }
-creerSoleil()
+
+let saisonAffichee = document.getElementById('Eté')
+function ChangerDeSaison(elm){
+    let saison = document.getElementById(elm)
+    saisonAffichee.style.visibility='hidden';
+    saison.style.visibility='visible';
+    saisonAffichee=saison;
+}
+
+
 const mto = document.querySelector('.meteo_du_jour')
 const titre = document.querySelector('#titre')
 const nav = document.querySelector('#entete')
 window.addEventListener('scroll', fixNav);
+
+const heiyt = document.getElementById('Horoscope').offsetHeight;
+let divs = document.getElementsByClassName("en-plus")
+for (i=0; i<divs.length; i++){
+    divs[i].style.height = heiyt.toString()+'px'
+}
+
+const quands = ["dmat", "dapm", "dsoi","admat", "adapm", "adsoi","ddmat", "ddapm", "ddsoi"];
+let request = new XMLHttpRequest();
+request.open("GET", "./météo.json");
+request.responseType = "json";
+request.send();
+request.onload = function () {
+    var previ = request.response;
+    Traitement(previ)}
+
+function Traitement(previ){
+    let minidiv = document.getElementsByClassName('image');
+    for(i=0; i<quands.length; i++){
+        //Récupération de la prévision le jour.
+        if (quands[i].slice(0,2)!= "ad" && quands[i].slice(0,2)!= "dd"){
+            let previprécise = previ.demain;
+            if (quands[i].slice(1)=== "mat"){
+                let previplusprécise = previprécise.matin;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+            else if (quands[i].slice(1)=== "apm"){
+                let previplusprécise = previprécise.aprèsmidi;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+            else if (quands[i].slice(1)=== "soi"){
+                let previplusprécise = previprécise.soir;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+        }
+        else if (quands[i].slice(0,2)=== "ad"){
+            let previprécise = previ.aprèsdemain;
+            if (quands[i].slice(2)=== "mat"){
+                let previplusprécise = previprécise.matin;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+            else if (quands[i].slice(2)=== "apm"){
+                let previplusprécise = previprécise.aprèsmidi;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+            else if (quands[i].slice(2)=== "soi"){
+                let previplusprécise = previprécise.soir;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+        }
+        else if (quands[i].slice(0,2)=== "dd"){
+            let previprécise = previ.aprèsaprèsdemain;
+            if (quands[i].slice(2)=== "mat"){
+                let previplusprécise = previprécise.matin;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+            else if (quands[i].slice(2)=== "apm"){
+                let previplusprécise = previprécise.aprèsmidi;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+            else if (quands[i].slice(2)=== "soi"){
+                let previplusprécise = previprécise.soir;
+                minidiv[i].src = './Images/'+previplusprécise.Temps+'.png'
+            }
+        }
+        console.log(minidiv[i])
+    };}
